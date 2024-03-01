4009716b11ed198b23c73a23226d97809df005b6
Samuel Gesang <sgesang@mailo.com>
2024-03-01 22:28:01 +0100

    improve skeleton loader

--- components/Styled/CardSkeleton.js
+++ components/Styled/CardSkeleton.js
@@ -5,7 +5,7 @@ export default function CardSkeleton() {
 }
 
 const StyledCardSkeleton = styled.li`
-  background-color: var(--color-lightgrey);
+  /* background-color: var(--color-lightgrey); */
   list-style-type: none;
   width: 333px;
   height: 123px;
@@ -15,4 +15,20 @@ const StyledCardSkeleton = styled.li`
   border: 1px solid var(--color-lightgrey);
   opacity: 0.3;
   box-shadow: 0 0 8px rgb(0 0 0 / 50%);
+  background: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0),
+      rgba(255, 255, 255, 0.5) 50%,
+      rgba(255, 255, 255, 0) 80%
+    ),
+    var(--color-lightgrey);
+  background-repeat: repeat-y;
+  background-size: 50px 500px;
+  background-position: 0 0;
+  animation: shine 1s infinite;
+  @keyframes shine {
+    to {
+      background-position: 100% 0;
+    }
+  }
 `;
